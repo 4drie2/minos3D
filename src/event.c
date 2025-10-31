a0591489b6e7430716515fa66aa05f0df2a3d4bb
Adrien 42 <adr.bidaux@gmail.com>
2025-10-31 14:22:36 +0100

    touch mac -> linux

--- src/event.c
+++ src/event.c
@@ -6,7 +6,7 @@
 /*   By: abidaux <abidaux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 20:44:07 by abidaux           #+#    #+#             */
-/*   Updated: 2025/10/31 14:12:33 by abidaux          ###   ########.fr       */
+/*   Updated: 2025/10/31 14:22:25 by abidaux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,24 @@
 /*
 ** key_press : gère les touches du clavier
 **
-** MacOS keycodes :
-** 53 = ESC
-** 13 = W (avancer)
-** 1 = S (reculer)
-** 123 = Flèche gauche (tourner)
-** 124 = Flèche droite (tourner)
+** Linux keycodes (X11) :
+** 65307 = ESC
+** 119 = W (avancer)
+** 115 = S (reculer)
+** 65361 = Flèche gauche (tourner)
+** 65363 = Flèche droite (tourner)
 */
 int	key_press(int keycode, t_data *data)
 {
-	if (keycode == 53)
+	if (keycode == 65307) // ESC
 		close_window(data);
-	else if (keycode == 13)
+	else if (keycode == 119) // W
 		move_forward(data);
-	else if (keycode == 1)
+	else if (keycode == 115) // S
 		move_backward(data);
-	else if (keycode == 123)
+	else if (keycode == 65361) // Left arrow
 		rotate_left(data);
-	else if (keycode == 124)
+	else if (keycode == 65363) // Right arrow
 		rotate_right(data);
 
 	return (0);
